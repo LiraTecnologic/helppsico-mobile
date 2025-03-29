22cad605fbd231e2d1e368d3cdfb9ba4d04d2377
HeitorHrecek <heitorhrecek@outlook.com>
2025-03-29 14:39:50 -0300

    #MTR08:feat/Adicionando o menu reutilizavel na tela meu painel

--- helppsico_mobile/lib/presentation/screens/dashboard_screen.dart
+++ helppsico_mobile/lib/presentation/screens/dashboard_screen.dart
@@ -10,6 +10,7 @@ import '../../data/mock_documents.dart';
 import '../../data/mock_sessions.dart';
 import '../../data/models/document_model.dart';
 import '../../data/models/session_model.dart';
+import '../widgets/drawer/custom_drawer.dart';
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
@@ -46,7 +47,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
       final documents = await _documentRepository.getDocuments();
       if (documents.isNotEmpty) {
         setState(() {
-          _lastDocument = documents.first; // O primeiro documento é o mais recente
+          _lastDocument = documents.first; 
         });
       }
     } catch (e) {
@@ -130,7 +131,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ),
         ],
       ),
-      drawer: _buildDrawer(context),
+      drawer: const CustomDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -213,125 +214,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
           ],
         ),
       ),
-    );
-  }
-
-  Widget _buildDrawer(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.7,
-      child: Drawer(
-        child: Container(
-          color: AppTheme.primaryColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  const SizedBox(height: 20), 
-                  Stack(
-                    children: [
-                      Align(
-                        alignment: Alignment.center,
-                        child: const Text(
-                          "Menu",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 32,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        bottom: 20,
-                        right: 0,
-                        child: IconButton(
-                          icon: const Icon(Icons.menu, color: Colors.white, size: 25),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24), 
-                  _buildDrawerItem(
-                    icon: Icons.home,
-                    title: "Meu Painel",
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                  _buildDrawerItem(
-                    icon: Icons.calendar_today,
-                    title: "Sessões",
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const SessionsPage()),
-                      );
-                    },
-                  ),
-                  _buildDrawerItem(
-                    icon: Icons.description,
-                    title: "Documentos",
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const DocumentsScreen()),
-                      );
-                    },
-                  ),
-                  _buildDrawerItem(
-                    icon: Icons.notifications,
-                    title: "Notificações",
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const NotificationsScreen()),
-                      );
-                    },
-                  ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: _buildDrawerItem(
-                  icon: Icons.logout,
-                  title: "Sair",
-                  onTap: () {
-                    Navigator.pop(context);
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => const LoginScreen()),
-                    );
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildDrawerItem({
-    required IconData icon,
-    required String title,
-    required VoidCallback onTap,
-  }) {
-    return ListTile(
-      leading: Icon(icon, color: Colors.white),
-      title: Text(
-        title,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 16,
-        ),
-      ),
-      onTap: onTap,
     );
   }
 }
