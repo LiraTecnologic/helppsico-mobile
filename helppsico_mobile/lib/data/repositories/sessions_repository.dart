5ad23c0177ece552d5303cc96f265990ffd76145
RAULALBERT001 <albertiraulsergio@gmail.com>
2025-04-05 17:21:48 -0300

    #MTR13:chore/retirando o print statement

--- helppsico_mobile/lib/data/repositories/sessions_repository.dart
+++ helppsico_mobile/lib/data/repositories/sessions_repository.dart
@@ -4,14 +4,13 @@ import 'package:http/http.dart' as http;
 
 
 class SessionRepository {
-  final String baseUrl = 'http://localhost:7000'; //Adicione o endpoint da api aqui :)
+  final String baseUrl = 'http://localhost:7000';
 
   Future<List<SessionModel>> getSessions() async {
     try {
       final response = await http.get(Uri.parse('$baseUrl/sessions'));
       if (response.statusCode == 200) {
         final List<dynamic> jsonList = json.decode(response.body);// o json retornado tem que ser do tipo lista
-        print(jsonList);
         return jsonList.map((json) => SessionModel.fromJson(json)).toList();
 
       } else if (response.statusCode == 404) {
